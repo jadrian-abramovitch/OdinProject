a8faa7ddaa5193e7bf77a907a2403c2958bd282d
Jadrian Maxwell Abramovitch <jmabramovitch@uwaterloo.ca>
2021-08-10 15:03:20 -0400

    finished bare bones version of game

--- tictactoe/js/tictactoe.js
+++ tictactoe/js/tictactoe.js
@@ -1,5 +1,5 @@
 const GameBoard = (() => {
-    const board = [[0,0,0], [0,0,0], [0,0,0]];
+    let board = [[0,0,0], [0,0,0], [0,0,0]];
     const checkIfPositionOpen = (x, y) => board[x][y] === 0;
     const playPosition = (playerId, x, y) => {
         if (checkIfPositionOpen(x,y)) {
@@ -10,6 +10,10 @@ const GameBoard = (() => {
         }
         DisplayController.updateBoard();
         //check for win and do something about it
+
+        if (checkForSolution()) {
+            DisplayController.declareWinner(playerId);
+        }
     }
     const getValueOfSquare = (x,y) => {
         if (board[x][y] === 0) {
@@ -19,30 +23,42 @@ const GameBoard = (() => {
         }
     }
 
+    const clearBoard = () => {
+        board = [[0,0,0], [0,0,0], [0,0,0]];
+        DisplayController.updateBoard();
+    }
+
     const checkForSolution = () => {
         //check for horizontal solutions
         for (let i = 0; i < board.length; i++) {
-            for (let j = 1; j < board.length[0]; j++) {
-                if (board[i][j] !== board[i][j-1]) { break;}
+            let matches = 0;
+            for (let j = 1; j < board[0].length; j++) {
+                if (board[i][j] === 0) { break; }
+                if (board[i][j] !== board[i][j-1]) { break; }
+                matches += 1;
             }
-            return(true);
+            if (matches === 2) { return(true); }
         }
 
         //check for vertical solutions
-        for (let j = 0; j < board.length[0]; i++) {
+        for (let j = 0; j < board[0].length; j++) {
+            let matches = 0;
             for (let i = 1; i < board.length; i++) {
-                if (board[i][j] !== board[i-1][j]) { break;}
+                if (board[i][j] === 0) { break; }
+                if (board[i][j] !== board[i-1][j]) { break; }
+                matches += 1;
             }
-            return(true);
+            if (matches === 2) { return(true); }
         }
 
         //check for diagonal solutions (top left to bottom right)
         for (let i = 1; i < board.length-1; i++) {
+            if (board[i][i] === 0) { break; }
             if (board[i][i] === board[i-1][i-1] && board[i][i] === board[i+1][i+1]) { return(true); }
         }
 
         //check for diagonal solutions (top left to bottom right)
-        if (board[2][0] === board[1][1] && board[0][2] === board[1][1]) { return(true); }
+        if (board[2][0] === board[1][1] && board[0][2] === board[1][1] && board[1][1] !== 0) { return(true); }
     }
 
     return {
@@ -50,6 +66,7 @@ const GameBoard = (() => {
         board,
         getValueOfSquare,
         checkForSolution,
+        clearBoard,
     };
 })();
 
@@ -62,10 +79,15 @@ const Player = (playerId) => {
 }
 
 const Turn = (() => {
-    let playerTurn = 1;
+    let playerTurn = -1;
+    const playerMapping = {
+        "1" : "X",
+        "-1" : "Y"
+    };
     const getPlayerTurn = () => {
         nextTurn();
-        return(playerTurn);
+        console.log(playerMapping[playerTurn]);
+        return(playerMapping[playerTurn]);
     }
     const nextTurn = () => {
         playerTurn*=-1;
@@ -78,6 +100,7 @@ const Turn = (() => {
 const DisplayController = (() => {
     const displayBoard = () => {
         let body = document.body;
+        resetButton();
         let tbl = document.createElement('table');
         tbl.setAttribute("id", "game board");
 
@@ -104,19 +127,41 @@ const DisplayController = (() => {
         body.appendChild(tbl);
     }
 
-    const deleteBoard = () => {
-        //TODO: implement
-        console.log("delete board");
-    }
+    const deleteBoard = () => { 
+        let table = document.getElementById("game board");
+            if (table) {
+                table.remove();
+            }
+            console.log("delete board");
+        }
 
     const updateBoard = () => {
         deleteBoard();
         displayBoard();
     }
 
+    const declareWinner = (playerId) => {
+        alert("Player " + playerId + " Wins!");
+    }
+
+    const resetButton = () => {
+        let body = document.body;
+        const existingButton = document.getElementById("Reset Button");
+        if (!existingButton) {
+            let resetButton = document.createElement("button");
+            resetButton.innerHTML = "Reset Board";
+            resetButton.id = "Reset Button"
+            resetButton.addEventListener("click", function() {
+                GameBoard.clearBoard();
+            })
+            body.appendChild(resetButton);
+        }
+    }
+
     return {
         displayBoard,
         updateBoard,
+        declareWinner,
     };
 })();
 
